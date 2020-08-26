f3e21d2d1929d228e2c3ee7eeb5cac9ed4b04c14
Sarah McClain <43018457+smcclain56@users.noreply.github.com>
2020-08-26 18:13:48 -0500

    added directory state, collection state to redux and created a new collection overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-    render(){
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-             collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>);
-    }
-}
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        <CollectionsOverview />
+    </div>
+) 
+
 
 export default ShopPage;
---
+++ src/redux/directory/directory.selectors.js
@@ -0,0 +1,9 @@
+import { createSelector } from 'reselect';
+
+const selectDirecotry = state => state.directory;
+
+export const selectDirectorySections = createSelector(
+    [selectDirecotry],
+    directory => directory.sections
+);
+
